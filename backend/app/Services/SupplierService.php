801522d603bb4e91fb39280147601768cfc27131
belloLucas <lucasbellodev@gmail.com>
2025-07-04 17:49:28 -0300

    feat: creating SupplierService

---
+++ backend/app/Services/SupplierService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\SupplierRepository;
+
+class SupplierService
+{
+    public function __construct(protected SupplierRepository $supplierRepository) {}
+
+    public function getAllSuppliers()
+    {
+        return $this->supplierRepository->getAllSuppliers();
+    }
+
+    public function getSupplierById($id)
+    {
+        return $this->supplierRepository->getSupplierById($id);
+    }
+
+    public function getSupplierByDocument(string $documentType, string $documentNumber)
+    {
+        return $this->supplierRepository->getSupplierByDocument($documentType, $documentNumber);
+    }
+
+    public function createSupplier(array $data)
+    {
+        return $this->supplierRepository->createSupplier($data);
+    }
+
+    public function updateSupplier($id, array $data)
+    {
+        return $this->supplierRepository->updateSupplier($id, $data);
+    }
+
+    public function deleteSupplier($id)
+    {
+        return $this->supplierRepository->deleteSupplier($id);
+    }
+}
