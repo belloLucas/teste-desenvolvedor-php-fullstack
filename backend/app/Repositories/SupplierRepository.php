7d78a8c99d87e5d0e26e553e15b793e5ec303514
belloLucas <lucasbellodev@gmail.com>
2025-07-04 17:49:14 -0300

    feat: creating SupplierRepository

---
+++ backend/app/Repositories/SupplierRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Supplier;
+
+class SupplierRepository
+{
+    public function __construct(protected Supplier $supplier) {}
+
+    public function getAllSuppliers()
+    {
+        return $this->supplier->all();
+    }
+
+    public function getSupplierById($id)
+    {
+        return $this->supplier->find($id);
+    }
+
+    public function getSupplierByDocument(string $documentType, string $documentNumber)
+    {
+        return $this->supplier->where('document_type', $documentType)
+                              ->where('document_number', $documentNumber)
+                              ->first();
+    }
+
+    public function createSupplier(array $data)
+    {
+        return $this->supplier->create($data);
+    }
+
+    public function updateSupplier($id, array $data)
+    {
+        $supplier = $this->getSupplierById($id);
+        if ($supplier) {
+            $supplier->update($data);
+            return $supplier;
+        }
+        return null;
+    }
+
+    public function deleteSupplier($id)
+    {
+        $supplier = $this->getSupplierById($id);
+        if ($supplier) {
+            $supplier->delete();
+            return true;
+        }
+        return false;
+    }
+}
