b533a616fce75d16af0d77b5a4810305e4547622
belloLucas <lucasbellodev@gmail.com>
2025-07-04 17:01:00 -0300

    feat: creating api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'suppliers'], function () {
+    Route::get('/', [\App\Http\Controllers\SupplierController::class, 'index']);
+    Route::get('/{id}', [\App\Http\Controllers\SupplierController::class, 'show']);
+    Route::get('/by-document', [\App\Http\Controllers\SupplierController::class, 'showByDocument']);
+    Route::post('/', [\App\Http\Controllers\SupplierController::class, 'store']);
+    Route::put('/{id}', [\App\Http\Controllers\SupplierController::class, 'update']);
+    Route::delete('/{id}', [\App\Http\Controllers\SupplierController::class, 'destroy']);
+});
