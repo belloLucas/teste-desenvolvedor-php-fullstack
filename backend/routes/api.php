fe2a784eb63f7c5ba20d6142a7fe9315e67b5a80
belloLucas <lucasbellodev@gmail.com>
2025-07-04 17:48:22 -0300

    fix: fixing route order

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,9 +9,9 @@ Route::get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'suppliers'], function () {
     Route::get('/', [\App\Http\Controllers\SupplierController::class, 'index']);
+    Route::get('/document', [\App\Http\Controllers\SupplierController::class, 'showByDocument']);
     Route::get('/{id}', [\App\Http\Controllers\SupplierController::class, 'show']);
-    Route::get('/by-document', [\App\Http\Controllers\SupplierController::class, 'showByDocument']);
     Route::post('/', [\App\Http\Controllers\SupplierController::class, 'store']);
-    Route::put('/{id}', [\App\Http\Controllers\SupplierController::class, 'update']);
+    Route::patch('/{id}', [\App\Http\Controllers\SupplierController::class, 'update']);
     Route::delete('/{id}', [\App\Http\Controllers\SupplierController::class, 'destroy']);
 });
