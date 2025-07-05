3b575e0744be1816b5fa651de062592462371475
belloLucas <lucasbellodev@gmail.com>
2025-07-04 21:55:44 -0300

    fix: fixing example test to refresh db

--- backend/tests/Feature/ExampleTest.php
+++ backend/tests/Feature/ExampleTest.php
@@ -1,7 +1,10 @@
 <?php
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
 
 it('returns a successful response', function () {
-    $response = $this->get('/');
+    $response = $this->get('/api/suppliers');
 
     $response->assertStatus(200);
 });
