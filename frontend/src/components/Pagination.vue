247049e38446f3e4b4c616d2f9e7cbc212dd2128
belloLucas <lucasbellodev@gmail.com>
2025-07-05 10:50:00 -0300

    feat: creating pagination component

---
+++ frontend/src/components/Pagination.vue
@@ -0,0 +1,53 @@
+<script setup></script>
+
+<template>
+  <div
+    class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6 mt-4"
+  >
+    <div class="flex-1 flex justify-between sm:hidden">
+      <button
+        class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
+      >
+        Anterior
+      </button>
+      <button
+        class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
+      >
+        Próximo
+      </button>
+    </div>
+
+    <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+      <div>
+        <p class="text-sm text-gray-700">
+          Mostrando <span class="font-medium">1</span> a
+          <span class="font-medium">10</span> de
+          <span class="font-medium">100</span> resultados
+        </p>
+      </div>
+      <div>
+        <nav class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px">
+          <button
+            class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
+          >
+            &lt;
+          </button>
+
+          <button
+            class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 cursor-pointer"
+          >
+            1
+          </button>
+
+          <button
+            class="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
+          >
+            &gt;
+          </button>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
