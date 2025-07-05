731dbdc8a325e7fb74e5850e670a032e90b78411
belloLucas <lucasbellodev@gmail.com>
2025-07-05 10:50:11 -0300

    feat: structuring all components together

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,41 @@
 <script setup>
 import Header from "./components/Header.vue";
+import SupplierFilters from "./components/SupplierFilters.vue";
+import SupplierList from "./components/SupplierList.vue";
+import SupplierForm from "./components/SupplierForm.vue";
+
+import { ref } from "vue";
+
+const showForm = ref(false);
+
+const openForm = () => {
+  showForm.value = true;
+};
+
+const closeForm = () => {
+  showForm.value = false;
+};
 </script>
 
 <template>
-  <Header />
+  <main class="min-h-screen bg-gray-50">
+    <Header @open-form="openForm" />
+
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <SupplierFilters />
+      <SupplierList />
+    </div>
+
+    <div
+      v-if="showForm"
+      class="fixed inset-0 bg-black bg-black/50 z-40 flex justify-center items-center"
+      @click.self="closeForm"
+    >
+      <div class="w-full max-w-3xl max-h-[90vh] overflow-y-auto">
+        <SupplierForm @close-form="closeForm" />
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped></style>
