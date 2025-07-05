aaefbacc4db1f0b6eea7789ae7d0006316bdaf60
belloLucas <lucasbellodev@gmail.com>
2025-07-05 11:28:25 -0300

    feat: adding logic to open fetch cnpj modal

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,10 +3,12 @@ import Header from "./components/Header.vue";
 import SupplierFilters from "./components/SupplierFilters.vue";
 import SupplierList from "./components/SupplierList.vue";
 import SupplierForm from "./components/SupplierForm.vue";
+import DocSearch from "./components/DocSearch.vue";
 
 import { ref } from "vue";
 
 const showForm = ref(false);
+const showDocSearch = ref(false);
 
 const openForm = () => {
   showForm.value = true;
@@ -15,11 +17,19 @@ const openForm = () => {
 const closeForm = () => {
   showForm.value = false;
 };
+
+const openDocSearch = () => {
+  showDocSearch.value = true;
+};
+
+const closeDocSearch = () => {
+  showDocSearch.value = false;
+};
 </script>
 
 <template>
   <main class="min-h-screen bg-gray-50">
-    <Header @open-form="openForm" />
+    <Header @open-form="openForm" @open-doc-search="openDocSearch" />
 
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <SupplierFilters />
@@ -34,6 +44,14 @@ const closeForm = () => {
       <div class="w-full max-w-3xl max-h-[90vh] overflow-y-auto">
         <SupplierForm @close-form="closeForm" />
       </div>
+    </div>
+
+    <div
+      v-if="showDocSearch"
+      class="fixed inset-0 bg-black bg-black/50 z-40 flex justify-center items-center"
+      @click.self="closeDocSearch"
+    >
+      <DocSearch @close-doc-search="closeDocSearch" />
     </div>
   </main>
 </template>
